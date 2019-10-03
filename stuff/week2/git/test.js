c0498272d95b4d4c29b4bd4462a073e22a6fac5d
Cristiam Da Silva <cristiamdasilva@Cristiams-MacBook-Pro.local>
2019-10-03 19:41:14 +0200

    added template string to console log

--- stuff/week2/git/test.js
+++ stuff/week2/git/test.js
@@ -2,6 +2,6 @@ var beatles = ['John', 'Ringo', 'George', 'Paul'];
 
 function printArr(arr){
   for(var i=0 ; i<arr.length ; i++) {
-    console.log('The name is: '+arr[i]);
+    console.log(`The name is: ${arr[i]}`);
   }
 }
