649f19e228ab4adb8386cc3bf3dce6f4ae66d5bc
eugferman <eugferman@gmail.com>
2019-10-05 13:52:22 +0200

    Ejercicios js

---
+++ staff/eugenio/week2/saturday.js
@@ -0,0 +1,47 @@
+// 1. EJERCICIO PARA EVALUAR MAYOR O MENOR
+//var valor1 = prompt("Introduce el primer valor", "0");
+//var valor2 = prompt("Introduce el primer valor", "0");
+
+function largerInteger(num1, num2){
+  //parseamos con parseInt las variables que vienen por argumento ya que nos vienen como String
+  if(parseInt(num1) > parseInt(num2)){
+    alert(num1+" es mayor que "+num2);
+  }else if(parseInt(var1) < parseInt(num2)){
+    alert(num1+" es menor que "+num2);
+  }else{
+    alert("Los valores son iguales");
+  }
+}
+
+//largerInteger(valor1, valor2);
+
+// 2. EJERCICIO PARA EVALUAR SI EL VALOR PASADO ES NEGATIVO
+var valor3 = prompt("Introduce el primer valor", "0");
+var valor4 = prompt("Introduce el primer valor", "0");
+var valor5 = prompt("Introduce el primer valor", "0");
+function signProduct(num3, num4, num5){
+  var resultado = num3*num4*num5;
+  if(resultado < 0){
+    alert(resultado+' es negativo');
+  }else{
+    alert(resultado+' es positivo');
+  }
+}
+signProduct(valor3, valor4, valor5);
+
+// 3. EJERCICIO PARA ORDENAR VALORES
+//var valor6 = prompt("Introduce el primer valor", "0");
+//var valor7 = prompt("Introduce el segundo valor", "0");
+//var valor8 = prompt("Introduce el tercer valor", "0");
+
+function sortNumbers(num6, num7, num8){
+  var arr = [num6, num7, num8]
+  arr.sort();
+  alert(arr);
+}
+
+//sortNumbers(valor6, valor7, valor8);
+
+// 4. MUESTRA EL MAYOR NÚMERO DE TODOS
+
+
