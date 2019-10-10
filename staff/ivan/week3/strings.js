a3f91abf3b3f134935433e220712b15bcc9af41a
ivandax <ivandax_89@hotmail.com>
2019-10-10 20:55:38 +0200

    started working on string exercises

---
+++ staff/ivan/week3/strings.js
@@ -0,0 +1,58 @@
+function isString(input) {
+  return typeof input === 'string';
+
+}
+function isBlank(input) {
+  return input.length===0;
+
+}
+function stringToArray(input) {
+  var split = input.split(" ");
+  return split;
+
+}
+function truncate(input, number) {
+  var trunc = input.substring(0,number);
+  return trunc;
+}
+
+function abbrevName(input) {
+  return '';
+
+}
+function protect(input) {
+  return '';
+
+}
+function parameterize(input) {
+  return '';
+
+}
+function capitalizeFirst(input) {
+  return '';
+
+}
+function capitalizeWords(input) {
+  return '';
+
+}
+function swapcase(input) {
+  return '';
+
+}
+function camelize(input) {
+  return '';
+
+}
+function uncamelize(input) {
+  return '';
+
+}
+function repeat(input, n) {
+  return '';
+
+}
+function insert(input, insert, position) {
+  return '';
+
+}
