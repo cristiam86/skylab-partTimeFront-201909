d2dbb6c9cc1b26cca13408a8e5454745aa8d7554
Jesús Ramírez <jmramirez@intercomempresas.com>
2019-10-11 19:10:27 +0200

    save strings test

---
+++ staff/jramirez/week3/strings/strings.js
@@ -0,0 +1,74 @@
+function isString(input) {
+  if ( typeof input === 'string') return true;
+  return false;  
+}
+function isBlank(input) {
+  if ( input === '') return true;
+  return false;  
+}
+function stringToArray(input) {
+  array = input.split(' ');
+  return array;
+
+}
+function truncate(input, number) {
+  str = input.slice(0, number);
+  return str;
+}
+
+function abbrevName(input) {
+  var name = input.trim().split(" ");
+    if (name.length > 1) {
+        return (name[0] + " " + name[1].charAt(0) + ".");
+    }
+    return name[0];
+
+}
+function protect(input) {
+  var email = input.split("@");
+  var resultado = "";
+  if (email[0].length > 5) {
+    return resultado =  email[0].slice(0,5) + "..." + "@" + email[1];
+  } else {
+    return resultado =  email[0].slice(0,2) + "..." + "@" + email[1];
+  }
+}
+
+function parameterize(input) {
+  var parametros = input.toLowerCase();
+  for (var i=0; i < parametros.length+1; i++){
+    if (){
+      
+    }
+  }
+  return resultado;
+
+}
+function capitalizeFirst(input) {
+  return '';
+
+}
+function capitalizeWords(input) {
+  return '';
+
+}
+function swapcase(input) {
+  return '';
+
+}
+function camelize(input) {
+  return '';
+
+}
+function uncamelize(input) {
+  return '';
+
+}
+function repeat(input, n) {
+  return '';
+
+}
+function insert(input, insert, position) {
+  return '';
+
+}
