4e8f6f2cdc267dd239778ce46cbb91beb54ecb92
ivandax <ivandax_89@hotmail.com>
2019-10-11 18:05:14 +0200

    actualized string functions

--- staff/ivan/week3/strings.js
+++ staff/ivan/week3/strings.js
@@ -8,6 +8,7 @@ function isBlank(input) {
 }
 function stringToArray(input) {
   var split = input.split(" ");
+  //console.log(split);
   return split;
 
 }
@@ -17,33 +18,83 @@ function truncate(input, number) {
 }
 
 function abbrevName(input) {
-  return '';
-
+  var split = input.split(" ");
+  var new_text = "";
+  if(split.length>1){
+    new_text=split[0]+" "+split[1][0]+"."
+  } else{
+    new_text=split[0];
+  }
+  //console.log(new_text)
+  return new_text;
 }
+
 function protect(input) {
-  return '';
-
+  var protected = "";
+  //console.log(pos_of_at);
+  var user = input.split("@")[0];
+  //console.log(user)
+  if(user.length>=8){
+    protected = user.substr(0,5)+"...@"+input.split("@")[1];
+    //console.log(protected)
+  } else{
+    protected = user.substr(0,user.length-3)+"...@"+input.split("@")[1];
+    //console.log(protected)
+  }
+  return protected;
 }
+
+
 function parameterize(input) {
-  return '';
-
+  var clear_string = input.replace(/[^\w\s]|_/g, "").toLowerCase();
+  var into_array = clear_string.split(" ");
+  var parameter = "";
+  //console.log(into_array)
+  for(var i in into_array){
+    parameter += into_array[i] + "-";
+  }
+  //console.log(parameter.substr(0,parameter.length-1))
+  return parameter.substr(0,parameter.length-1); //remove the last -
 }
+
 function capitalizeFirst(input) {
-  return '';
+  var upper_letter = input[0].toUpperCase();
+  var new_string = upper_letter+input.substr(1,input.length).toLowerCase();
+  //console.log(new_string);
+  return new_string;
 
 }
 function capitalizeWords(input) {
-  return '';
+  var split = input.split(" ");
+  var new_string = "";
+  //console.log(split)
+  for (word in split){
+    new_string += split[word][0].toUpperCase()+split[word].substr(1,split[word].length)+" "
+  }
+  //console.log(new_string)
+  return new_string.substr(0,new_string.length-1);
 
 }
+
 function swapcase(input) {
-  return '';
-
+  var split = input.split("");
+  //console.log(split)
+  var new_string = '';
+  for(letter in split){
+    if(split[letter] === split[letter].toUpperCase()){
+      new_string += split[letter].toLowerCase();
+    } else if(split[letter] === split[letter].toLowerCase()){
+      new_string += split[letter].toUpperCase();
+    }
+  }
+  //console.log(new_string)
+  return new_string;
 }
+
 function camelize(input) {
   return '';
-
 }
+
 function uncamelize(input) {
   return '';
 
