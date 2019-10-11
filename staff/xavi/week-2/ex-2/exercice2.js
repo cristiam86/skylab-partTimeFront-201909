455f0831d0d21dc794b9d6d76d8ffda6e44f8e10
xavier pascual <xavierskylab@gmail.com>
2019-10-11 18:08:10 +0200

    merge from testing

---
+++ staff/xavi/week-2/ex-2/exercice2.js
@@ -0,0 +1,25 @@
+
+function isString(input) {
+    if(typeof input == 'number'){
+        return('number')
+    } else{
+        return('string')
+    }
+}
+
+/*
+    var input = 'ytred';
+    if(typeof input == 'number'){
+        console.log('es un numero')
+    } else{
+        console.log('es un string')
+    }
+*/
+
+
+
+
+
+
+
+
