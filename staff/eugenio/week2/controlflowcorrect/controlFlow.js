8329b888ca72749e98e246e35ccb83ef46a6c434
eugferman <eugferman@gmail.com>
2019-10-11 15:31:18 +0200

    controlflow y test completado

--- staff/eugenio/week2/controlflowcorrect/controlFlow.js
+++ staff/eugenio/week2/controlflowcorrect/controlFlow.js
@@ -128,25 +128,18 @@ function contructPattern(){
 
 // 8. CREAMOS UNA FUNCIÓN QUE NOS SUME LOS MULTIPLOS DE 3 Y 5 POR DEBAJO DE 1000
 
-function under1000(){
+function under1000(num){
   var mult3 = 0;
   var mult5 = 0;
-  for(var x =1; x <= 1000; x++){
-    var reset3 = 0;
-    var reset5 = 0;
-    if(x % 3 == 0){
-      reset3 = x;
-      mult3 = mult3 + reset3;
+  for(var x =1; x <= num; x++){
+    if(x % 3 === 0){
+      mult3 += x;
     }
-    if(x % 5 == 0){
-      reset5 = x;
-      mult5 = mult5 + reset5;
+    if(x % 5 === 0){
+      mult5 += x;
     }
   }
-  console.log("Suma total de los múltiples de 3: "+mult3);
-  console.log("Suma total de los múltiples de 5: "+mult5);
-  console.log("Suma total de los múltiples de 3 y 5: "+(mult3+mult5));
+  return mult3+mult5;
 }
 
-//under1000();
 
