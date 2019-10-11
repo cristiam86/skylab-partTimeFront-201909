3eeb1077cd2dda1caac7da7b2bd29614f5ce9275
Suneleta <elenhtakh@gmail.com>
2019-10-11 17:34:19 +0200

    Added string exercises

--- staff/eleni/week2/strings/strings.js
+++ staff/eleni/week2/strings/strings.js
@@ -1,56 +1,92 @@
 function isString(input) {
-  return typeof input === 'string';
-
+  if (typeof(input)==='string'){
+    return true;
+  } else{
+    return false;
+  }
 }
 function isBlank(input) {
-  return '';
-
+    return (!input || 0 === input.length);
 }
 function stringToArray(input) {
-  return '';
-
+    return (input.split(' '));
 }
 function truncate(input, number) {
-  return '';
-
+    if ((typeof(input) === 'string') && (number=>0)) {
+        return input.slice(0, number);
+  };
 }
 function abbrevName(input) {
-  return '';
-
+    var splitNames=input.trim().split(" ");
+    if(splitNames.length>1){
+      return (splitNames[0]+ " "+splitNames[1].charAt(0)+".")
+    }else{
+      return splitNames[0]
+    }
 }
-function protect(input) {
-  return '';
-
-}
+  function protect(input) {
+    var splitEmail=input.split("@");
+    part1=splitEmail[0];
+    part2=splitEmail[1];
+    var result=part1.split("_");
+    if(result.length>1){
+      return (result[0]+"...@"+part2)
+    }else if(result.length===1){
+      return (result[0].slice(0,2)+"...@"+part2)
+    }
+  }
 function parameterize(input) {
-  return '';
+      return input.trim().toLowerCase().replace(/[^a-zA-Z0-9 -]/, "").replace(/\s/g, "-").replace("'","").replace(".","");
 
 }
 function capitalizeFirst(input) {
-  return '';
-
+    minString=input.toLowerCase();
+    return minString.charAt(0).toUpperCase()+minString.slice(1);
 }
 function capitalizeWords(input) {
-  return '';
-
+    var splitStr = input.toLowerCase().split(' ');
+   for (var i = 0; i < splitStr.length; i++) {
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+   }
+   return splitStr.join(' '); 
 }
 function swapcase(input) {
-  return '';
+    var result = "";
+    for(var i = 0; i<input.length; i++){
+        if(input[i] === input[i].toLowerCase()){
+            result += input[i].toUpperCase();
+        }else {
+            result += input[i].toLowerCase();
+        }
+    }
+    return result;
 
 }
 function camelize(input) {
-  return '';
-
+   var splitStr = input.split(' ');
+   for (var i = 0; i < splitStr.length; i++) {
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+   }
+   return splitStr.join(''); 
 }
-function uncamelize(input) {
-  return '';
+
+
+function uncamelize(input,symbol) {
+  if(typeof(symbol) == "undefined") {
+    symbol = " ";
+  }
+  var input = input.replace(/[A-Z]/g, function (letter) 
+    {
+    return symbol + letter.toLowerCase();
+    });
+  return input.replace("/^" + symbol + "/", '');
 
 }
 function repeat(input, n) {
-  return '';
-
+    return  new Array(n + 1).join(input);
 }
 function insert(input, insert, position) {
-  return '';
+ 
+   return input.slice(0, position) + insert + input.slice(position);
 
 }
--- staff/eleni/week2/strings/strings.test.js
+++ staff/eleni/week2/strings/strings.test.js
@@ -72,7 +72,7 @@ if (result !== "") {
 }
 
 var result = truncate("word1 word2 word3", 6);
-if (result !== "word1 w") {
+if (result !== "word1 ") {
   errors += 1;
   console.error("ERROR: truncate expected to return word1 w");
 }
@@ -122,9 +122,9 @@ if (result !== "robin-singh-from-usa") {
 }
 
 var result = parameterize("My name is Robin Singh. I'm from USA.");
-if (result !== "my-name-es-robin-singh-im-from-usa") {
+if (result !== "my-name-is-robin-singh-im-from-usa") {
   errors += 1;
-  console.error("ERROR: parameterize expected to return my-name-es-robin-singh-im-from-usa");
+  console.error("ERROR: parameterize expected to return my-name-is-robin-singh-im-from-usa");
 }
 
 /////////////////
