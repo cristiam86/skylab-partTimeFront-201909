4c0c13cff71faa5a133e8c3dad930c84ffe2f4e1
Jesús Ramírez <jmramirez@intercomempresas.com>
2019-10-12 12:53:16 +0200

    ejercicios arrays

--- staff/jramirez/week3/arrays/arrays.js
+++ staff/jramirez/week3/arrays/arrays.js
@@ -1,4 +1,35 @@
 
-function isArray(arr) {
-  return Array.isArray(arr);
+function isArray(isArr) {
+  return isArr;
 }
+
+function joinElements(e) {
+  var resultado = e;
+  return resultado.join();
+}
+
+function arrayClone(e) {
+  return e;
+}
+
+function findDifferences(arrA, arrB) {
+  var isArr = [1, 2, 4, 0];
+  return isArr ;
+}
+
+var a = [1, 2, 4, 0];
+function sumAndProduct(a){
+  return a;
+}
+
+///ejercicio de suma recursiva
+
+//function sumaRecursiva(arr){
+ // if(arr.length <= 0) return 0;
+ // if(arr.length === 1){
+  //  return arr[0];
+ // }
+  //return arr.pop() + sumaRecursiva(arr);
+//}
+
+//sumaRecursiva([1,2,3]);
--- staff/jramirez/week3/arrays/arrays.test.js
+++ staff/jramirez/week3/arrays/arrays.test.js
@@ -1,5 +1,9 @@
 var errors = 0;
 
+
+/////////////////
+/// isArray ////
+////////////////
 var isArr = isArray([]);
 if(!isArr) {
   errors += 1;
@@ -9,8 +13,129 @@ if(!isArr) {
 var isArr = isArray("asd");
 if(typeof isArr === 'undefined' || isArr === true) {
   errors += 1;
-  console.error("2. isArray should have returned false");
+  console.error("1. isArray should have returned false");
 }
+
+/////////////////
+/// joinElements ////
+////////////////
+var isArr = joinElements(['John','George','Ringo','Paul']);
+if(isArr !== 'John,George,Ringo,Paul') {
+  errors += 1;
+  console.error("2. isArray should have returned John,George,Ringo,Paul");
+}
+
+/////////////////
+/// arrayClone ////
+////////////////
+var a = [1, 2, 4, 0];
+var isArr = arrayClone(a);
+isArr.pop();
+
+if (a.length === isArr.length && a.join() === isArr.join()) {
+  errors += 1;
+  console.error("3. arrayClone está cloná de la otra");
+}
+
+
+/////////////////
+/// findDifferences ////
+////////////////
+var arrA = [1, 2, 4, 0];
+var arrB = [1, 2, 4, 0];
+var isArr = findDifferences(arrA, arrB);
+if (arrA.join() === isArr.join() || arrB.join() === isArr.join() ) {
+  errors += 1;
+  console.error("4. Las Arrays son iguales colega");
+}
+
+/////////////////
+/// Sum and Product ////
+////////////////
+var isArr = sumAndProduct(a);
+var suma = 0;
+var product = 1;
+for (i = 0; i < isArr.length; i += 1) {
+  suma += isArr[i];
+  product *= isArr[i];
+}
+
+return suma;
+return product;
+if (suma.join() === isArr.join() || arrB.join() === isArr.join() ) {
+  errors += 1;
+  console.error("4. Las Arrays son iguales colega");
+}
+
+
+  
+/////////////////
+/// addItems ////
+////////////////
+
+
+
+/////////////////
+/// generateArrayLength ////
+////////////////
+
+
+
+/////////////////
+/// last ////
+////////////////
+
+
+/////////////////
+/// sortItems ////
+////////////////
+
+
+
+/////////////////
+/// getRandom ////
+////////////////
+
+
+/////////////////
+/// findDuplicates ////
+////////////////
+
+
+/////////////////
+/// mergeArrays ////
+////////////////
+
+
+/////////////////
+/// separateEven ////
+////////////////
+
+
+/////////////////
+/// mostFrequentItem ////
+////////////////
+
+
+/////////////////
+/// moveElement ////
+////////////////
+
+
+/////////////////
+/// swapCase ////
+////////////////
+
+
+/////////////////
+/// Print elements array ////
+////////////////
+
+
+
+/////////////////
+/// array_filled (string) ////
+////////////////
 
 
 if (errors){
