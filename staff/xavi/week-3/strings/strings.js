498bc2b17e41966f40b71717292269d940078b33
xavier pascual <xavierskylab@gmail.com>
2019-10-13 17:16:57 +0200

    protect email direcction

--- staff/xavi/week-3/strings/strings.js
+++ staff/xavi/week-3/strings/strings.js
@@ -93,6 +93,51 @@ truncate2()
 // EJERICIO 5 - abbrevName(input) Write a JavaScript function to convert a string in abbreviated form.
 
 
+
+
+// EJERCICIO - 7 Escriba una función para proteger una dirección de email
+
+
+ // creo una función y le paso el parametro input donde almaceno el email.
+ function protectEmail(input){
+    // en el input almaceno una dirección de email en string
+    var input = "xavier.pascual@gmail.com"
+    
+    // en la varaible email le digo que es igual a input, email, y que corte ese string en el signo @.
+    var email = input.split("@");
+  
+    // declaro la variable puntos que me servirá para sustituir a los caracteres. 
+    var points = '...';
+    //  La parte uno la indico que guarde el primer carcter de email y a partir del segundo que no lo guarde. 
+    var part1 = email[0].slice(0,1);
+    //  la parte dos guarda el email.
+    var part2 = email[1];
+  
+    // part1: escribo la primera letra y el resto lo elimino
+    // .concateno los tres puntos
+    // imprimo el @
+    // imprimo el resto del emnail
+    return(part1.concat(points,"@",part2));
+}
+
+protectEmail()
+
+// EJERCICIO 8 - Escriba una función de JavaScript para ocultar las direcciones de correo electrónico para protegerlas de usuarios no autorizados.
+
+
+function protectEmail(input){
+    var input = "xavier.pascual@gmail.com"
+    var email = input.split("@");
+    var points = '...';
+    var part1 = email[0].slice(0,1);
+    var part2 = email[1];
+    return(part1.concat(points,"@",part2));
+}
+
+protectEmail()
+
+
+
 /// EJERCICIO 9 - Escriba una función de JavaScript para concatenar una cadena dada n veces (el valor predeterminado es 1).
 
 function repeat(input,n){
