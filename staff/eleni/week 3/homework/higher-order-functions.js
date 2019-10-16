9a409ea10e09d81053ba4753f41eb4409faef3fc
Suneleta <elenhtakh@gmail.com>
2019-10-16 12:10:09 +0200

    Added high order functions

--- staff/eleni/week 3/homework/higher-order-functions.js
+++ staff/eleni/week 3/homework/higher-order-functions.js
@@ -1,8 +1,17 @@
 //// capitalCase
-function capitalCase(input){
-    input
-  var newInput = input.map(function(str){
-   str.charAt(0).toUpperCase() + str.slice(1);
-});
-return newInput;
+function capitalCase(input) {
+    input = input.toLowerCase()
+        .split(' ')
+        .map((first) => first.charAt(0).toUpperCase() + first.substring(1))
+        .join(' ');
+    return input;
+}
+
+/// someMonths
+function someMonths(input) {
+    input = input.toLowerCase()
+        .split(' ')
+        .map((first) => first.charAt(0).toUpperCase() + first.substring(1))
+        .join(' ');
+    return input;
 }
