21156e7a6b9e0a44d59ed7d7ad374572cd8ba33d
Suneleta <elenhtakh@gmail.com>
2019-10-16 10:27:58 +0200

    Added array exercises

--- staff/eleni/week 3/arrays/arrays.js
+++ staff/eleni/week 3/arrays/arrays.js
@@ -1,34 +1,34 @@
 ///isArray
-function isArray(input){
-    if(input instanceof Array){
+function isArray(input) {
+    if (input instanceof Array) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
 ///joinElements
-function joinElements(input){
+function joinElements(input) {
     return input.join(",");
 }
 
 
 ///arrayClone
-function arrayClone(input){
-    var copy=input.slice(0);
-    return copy;  
+function arrayClone(input) {
+    var copy = input.slice(0);
+    return copy;
 }
 
 ///findDifferences
-function findDifferences(array1,array2){
-    var temp=[];
-    for (var i in array1){
-        if(!array2.includes(array1[i])) temp.push(array1[i])
+function findDifferences(array1, array2) {
+    var temp = [];
+    for (var i in array1) {
+        if (!array2.includes(array1[i])) temp.push(array1[i])
     }
-    for(i in array2) {
-        if(!array1.includes(array2[i])) temp.push(array2[i]);
+    for (i in array2) {
+        if (!array1.includes(array2[i])) temp.push(array2[i]);
     }
-        return temp.sort((a,b) => a-b);
+    return temp.sort((a, b) => a - b);
 }
 /*
 function findDifferences(array1,array2){
@@ -42,27 +42,145 @@ function findDifferences(array1,array2){
 }*/
 
 ///// Sum and Product
-function SumAndProduct(input){
-    var sum=input.reduce((a, b) => a + b);
-    var product=input.reduce((a, b) => a * b);
-    return {sum:sum,product:product}
+function SumAndProduct(input) {
+    var sum = input.reduce((a, b) => a + b);
+    var product = input.reduce((a, b) => a * b);
+    return { sum: sum, product: product }
 }
 //// addItems
-function addItems(input,items){
-    return result=input.push(items);
+function addItems(items) {
+    var emptyArr = [];
+    var result = emptyArr.concat(items);
+    return result;
 }
 
 //// generateArrayLength
-function generateArrayLength(input1,input2){
-    var temp=[];
-    for (var i=input1; i<=input2; i++){
-        var result=temp.push(i);
-    } return temp;
+function generateArrayLength(input1, input2) {
+    var temp = [];
+    for (var i = input1; i <= input2; i++) {
+        var result = temp.push(i);
+    }
+    return temp;
 }
 
 ///last
-function getLast(input,n){
-    if(n>0){
-    return input.slice(Math.max(input.length - n, 1))
-    }else
+function getLast(input, n) {
+    if (n > 0) {
+        return input.slice(Math.max(input.length - n, 0))
+    } else {
+        return input.slice(-1);
+    }
 }
+
+//// sortItems
+function sort() {
+    return [].slice.call(arguments).sort(function(a, b) {
+        return a - b;
+    });
+}
+
+//// getRandom
+function getRandom() {
+    return [].slice.call(arguments)[Math.floor(Math.random() * arguments.length)];
+}
+
+///  findDuplicates
+function findDuplicates(input) {
+    return input.filter(function(a) {
+        return input.indexOf(a) !== input.lastIndexOf(a)
+    });
+}
+//// mergeArrays
+function mergeArrays(array1, array2) {
+    var merged = array1.concat(array2);
+    return merged.filter(function(a) {
+        return merged.indexOf(a) === merged.lastIndexOf(a)
+    });
+}
+/// separateEven
+
+///  mostFrequentItem
+
+
+/// moveElement
+function moveElement(arr, from, to) {
+    var element = arr[from];
+    arr.splice(from, 1);
+    arr.splice(to, 0, element);
+    return arr;
+}
+/// swapCase
+function swapCase(input) {
+    var newWord = [];
+
+    for (i = 0; i < input.length; i++) {
+        if (input[i] == input[i].toLowerCase()) {
+            newWord[i] = input[i].toUpperCase();
+        } else {
+            newWord[i] = input[i].toLowerCase();
+        }
+    }
+
+    return newWord.join("");
+}
+
+//// Print elements array
+
+/// array_filled (string)
+
+/// Sum squares
+
+/// removeDuplicate
+
+/// showChoices
+
+///showLeapYears
+function showLeapYears(st_year, end_year) {
+    function leap_year_range(st_year, end_year) {
+        var year_range = [];
+        for (var i = st_year; i <= end_year; i++) {
+            year_range.push(i);
+        }
+        var new_array = [];
+
+        year_range.forEach(
+            function(year) {
+                if (test_LeapYear(year))
+                    new_array.push(year);
+            });
+
+        return new_array;
+    }
+
+    function test_LeapYear(year) {
+        if ((year % 4 === 0 && year % 100 !== 0) || (year % 100 === 0 && year % 400 === 0)) {
+            return year;
+        } else {
+            return false;
+        }
+    }
+    return test_LeapYear
+}
+
+
+/// shuffleArray
+
+
+/// binarySearch
+///sumPositions
+////generateArraySteps
+////flattenArray
+///unionArray
+///removeFalsy
+///first
+///sortObjectsTitle
+///findPairSum
+///validValues
+///longestCommon
+///fillArray
+///removeElement
+///findArrayWithElement
+///emptyArray
+///getNthLargest
+///array_filled (numeric)
+///filterFalsy
--- staff/eleni/week 3/arrays/arrays.test.js
+++ staff/eleni/week 3/arrays/arrays.test.js
@@ -2,60 +2,104 @@ var errors = 0;
 
 ///isArray
 var isArr = isArray([]);
-if(!isArr) {
-  errors += 1;
-  console.error("1. isArray should have returned true");
+if (!isArr) {
+    errors += 1;
+    console.error("1. isArray should have returned true");
 }
 
 var isArr = isArray("asd");
-if(typeof isArr === 'undefined' || isArr === true) {
-  errors += 1;
-  console.error("2. isArray should have returned false");
+if (typeof isArr === 'undefined' ||  isArr === true) {
+    errors += 1;
+    console.error("2. isArray should have returned false");
 }
 
 /////joinElements
-var res = joinElements(['John','George','Ringo','Paul']);
+var res = joinElements(['John', 'George', 'Ringo', 'Paul']);
 if (res !== 'John,George,Ringo,Paul') {
-  errors += 1;
-  console.error("ERROR: join elements expected to return 'John,George,Ringo,Paul'");
+    errors += 1;
+    console.error("ERROR: join elements expected to return 'John,George,Ringo,Paul'");
 }
 
 ///arrayClone
-var res = arrayClone([1,2,4,0]);
-var res2= res;
-if(res2!==res){
-  errors += 1;
-  console.error("ERROR: array clone expected to return '1,2,4,0'");
+var res = arrayClone([1, 2, 4, 0]);
+var res2 = res;
+if (res2 !== res) {
+    errors += 1;
+    console.error("ERROR: array clone expected to return '1,2,4,0'");
 }
 
 ///findDifferences
-var res= findDifferences([1, 2, 3], [100, 2, 1, 10]); 
- /*  if(res!=='3, 10, 100') {
+var res = findDifferences([1, 2, 3], [100, 2, 1, 10]);
+/*  if(res!=='3, 10, 100') {
    errors += 1;
    console.error("ERROR: array clone expected to return 3,10,100");
 }*/
 
 ///// Sum and Product
-var res= SumAndProduct([2,3,4]); 
-   if((res.sum!==9) && (res.product!==24)) {
-   errors += 1;
-   console.error("ERROR: the sum should be 9 and the product 24");
+var res = SumAndProduct([2, 3, 4]);
+if ((res.sum !== 9) && (res.product !== 24)) {
+    errors += 1;
+    console.error("ERROR: the sum should be 9 and the product 24");
+}
+//// Add items 
+var res = addItems([2, 3, 4]);
+if (res.sum !== 1, 2, 3, 4) {
+    errors += 1;
+    console.error("ERROR: add items should display 1,2,3,4");
 }
 
 ////generateArrayLength
-var res= generateArrayLength(1,4); 
-    if (res.length!==4){
-   errors += 1;
-   console.error("ERROR: generate array length should return 1,2,3,4");
+var res = generateArrayLength(1, 4);
+if (res.length !== 4) {
+    errors += 1;
+    console.error("ERROR: generate array length should return 1,2,3,4");
 }
-var res= generateArrayLength(-6,-3); 
-    if (res.length!==4){
-   errors += 1;
-   console.error("ERROR: generate array length should return -6,-5,-4,-3");
+var res = generateArrayLength(-6, -3);
+if (res.length !== 4) {
+    errors += 1;
+    console.error("ERROR: generate array length should return -6,-5,-4,-3");
 }
 
-if (errors){
-  console.error(errors+" ERROR(S) FOUND");
+///getLast
+var res = getLast(([7, 9, 0, -2]));
+if (res[0] !== -2) {
+    errors += 1;
+    console.error("ERROR: get last should return -2");
+}
+var res = getLast([7, 9, 0, -2], 3);
+if (res[0] !== 9 || res[1] !== 0 || res[2] !== -2) {
+    errors += 1;
+    console.error("ERROR: get last should return 9,0,-2");
+}
+var res = getLast([7, 9, 0, -2], 6);
+if (res[0] !== 7 || res[1] !== 9 || res[2] !== 0 || res[3] !== -2) {
+    errors += 1;
+    console.error("ERROR: get last should return 7, 9, 0, -2");
+}
+
+/// sort
+var res = sort(3, 8, 7, 6, 5, -4, -3, 2, 1);
+if (res[0] !== -4 || res[1] !== -3 || res[2] !== 1 || res[3] !== 2 || res[4] !== 3 || res[5] !== 5 || res[6] !== 6 || res[7] !== 7 || res[8] !== 8) {
+    errors += 1;
+    console.error("ERROR: sort should return 3, 8, 7, 6, 5, -4, -3, 2, 1");
+}
+
+//// getRandom
+var res = getRandom(1, 2, 3);
+if (res !== 1 && res !== 2 && res !== 3) {
+    errors += 1;
+    console.error("ERROR: get random should return 1 or 2 or 3");
+}
+
+///  findDuplicates
+var res = findDuplicates(1, 2, 2, 3, 3);
+if (res !== 2 && res !== 3) {
+    errors += 1;
+    console.error("ERROR: find duplicates should return 2 and 3");
+}
+
+if (errors) {
+    console.error(errors + " ERROR(S) FOUND");
 } else {
-  console.log("CONGRATS!!!");
+    console.log("CONGRATS!!!");
 }
