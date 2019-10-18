ad8d5b5645159b9167ea49317a34ccf4f5959901
Sandrabg <sandra.garos@gmail.com>
2019-10-18 21:59:00 +0200

    recursion y objects week3

--- staff/Sandra/week3/objects/objects.js
+++ staff/Sandra/week3/objects/objects.js
@@ -20,62 +20,121 @@ var car = {
 car.printCar();
 
 //Stack
-
-var stack = [];
-
-
-function insertElement(element){
-    this.push(element);
+function Stack(){
+    this.elements = [];
+    this.insertElement = function (element){
+        this.elements.push(element);
+    }
+    this.getElement = function(){
+        this.elements.pop();
+    }
+    this.print = function (){
+        console.log(this.elements);
+    }
 }
 
-function getElement(){
-    this.pop();
-}
-
-function print(){
-    console.log(this);
-}
-
-insertElement(1);
-insertElement(2);
-insertElement(3);
-print();
-getElement();
-print();
-getElement();
-print();
-getElement();
-print();
+var myStack = new Stack();
+myStack.insertElement(1);
+myStack.insertElement(2);
+myStack.insertElement(3);
+myStack.print();
+myStack.getElement();
+myStack.print();
+myStack.getElement();
+myStack.print();
+myStack.getElement();
+myStack.print();
 
 //Queue
-var queue = [];
-
-function insertElement(element){
-    queue.unshift(element);
+function Queue(){
+    this.elements = [];
+    this.insertElement = function (element){
+        this.elements.unshift(element);
+    }
+    this.getElement = function(){
+        this.elements.pop();
+    }
+    this.print = function (){
+        console.log(this.elements);
+    }
 }
-
-function getElement(){
-    queue.pop();
-}
-
-function print(){
-    console.log(queue);
-}
-insertElement(1);
-insertElement(2);
-insertElement(3);
-print();
-getElement();
-print();
-getElement();
-print();
-getElement();
-print();
+var myQueue = new Queue();
+myQueue.insertElement(1);
+myQueue.insertElement(2);
+myQueue.insertElement(3);
+myQueue.print();
+myQueue.getElement();
+myQueue.print();
+myQueue.getElement();
+myQueue.print();
+myQueue.getElement();
+myQueue.print();
 
 //Linked List
-var node = {
-    prevElement,
-    data
+function node (index, element) {
+    this.prevElement = index;
+    this.data = element;
 }
 
+function LinkedList(){
+    this.elements =[]
+    this.insertElement = function (index,element) {
+        if (this.elements.length == 0){
+            this.elements.push(new node(0,element));
+        }else{
+        
+        }
+    }
+    this.getElement = function (index){
+        return this.elements[index].prevElement;
+    }
+    this.print = function (){
+        console.log(this.elements);
+    }
+}
+
+var myLinkedList = new LinkedList();
+
+
 //The blockchain
+var hash = function(s) {
+    var a = 1, c = 0, h, o;
+    if (s) {
+        a = 0;
+        for (h = s.length - 1; h >= 0; h--) {
+            o = s.charCodeAt(h);
+            a = (a<<6&268435455) + o + (o<<14);
+            c = a & 266338304;
+            a = c!==0?a^c>>21:a;
+        }
+    }
+    return String(a);
+};
+
+function block(number,data, prevHash = ''){
+    this.number = number;
+    this.timestamp = new Date();
+    this.prevHash = prevHash;
+    this.hash =  hash(this.number + this.timestamp);
+    this.data=data;
+}
+
+function blockchain(){
+    this.blocks = [];
+    this.addBlock = function (data){
+        if (this.blocks.length == 0){
+          this.blocks.push(new block(0,'Bloque0'));
+        }
+          var prevBlock = this.blocks.length-1;
+          var newBlock = new block(prevBlock+1,data,this.blocks[prevBlock].hash);
+          this.blocks.push(newBlock);
+    }
+    this.isValidChain = function (){
+       
+    }
+}
+
+var myBlockChain = new blockchain();
+myBlockChain.addBlock("Bloque1");
+myBlockChain.addBlock("Bloque2");
+console.log(myBlockChain.blocks);
--- staff/Sandra/week3/recursion/recursion.js
+++ staff/Sandra/week3/recursion/recursion.js
@@ -1,6 +1,13 @@
 //removeRepeatChars
+var arr='aabcbcb';
+function removeRepeatChars(arr){
+    if (arr.length <= 1) return arr;
+    if (arr.charAt(0) == arr.charAt(1)) return removeRepeatChars(arr.substring(1));
+    else return arr.charAt(0)+removeRepeatChars(arr.substring(1));  
+}
 
-//removeRepeatChars("aabcbcb");
+
+console.log("resultado: " + removeRepeatChars(arr));
 
 //fibonacci
 function fibonacci(n){
