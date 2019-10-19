cba2b7f0c918975071fb0576be39a49c6a480c9e
Xavier Pascual Betrán <xavierpascualbetran@iMac-de-Xavier.local>
2019-10-19 21:49:24 +0200

    homework

--- staff/xavi/methods/methods.js
+++ staff/xavi/methods/methods.js
@@ -1,3 +1,35 @@
 var words = ['spray', 'seisletras', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 const result = words.filter(word => word.length > 6);
 console.log(result);
+
+
+
+/******************************************************************************************** CONCAT */
+// METODO .CONCAT
+// .concat()
+// Se utiliza para unir dos o mas arrays.
+// NO modifica los array existentes, sino que devuelve un nuevo array.
+
+
+function concatenar(concat1, concat2){
+
+    var result = concat1.concat(concat2);
+    return(result)
+}
+
+concatenar('Hola ', 'Mundo')
+// 'Hola Mundo'
+
+
+
+
+
+/*************************************************************************************** .FILTER */
+// METODO .FILTER
+// .filter
+
+
+function filtro(result){
+
+    
+}
--- staff/xavi/week-3/arrays/array.js
+++ staff/xavi/week-3/arrays/array.js
@@ -273,61 +273,7 @@ function sumsquare(array) {
 
 
 
-///isArray
-function isArray(input) {
-  if (input instanceof Array) {
-      return true;
-  } else {
-      return false;
-  }
-}
 
-///joinElements
-function joinElements(input) {
-  return input.join(",");
-}
-
-
-///arrayClone
-function arrayClone(input) {
-  var copy = input.slice(0);
-  return copy;
-}
-
-///findDifferences
-function findDifferences(array1, array2) {
-  var temp = [];
-  for (var i in array1) {
-      if (!array2.includes(array1[i])) temp.push(array1[i])
-  }
-  for (i in array2) {
-      if (!array1.includes(array2[i])) temp.push(array2[i]);
-  }
-  return temp.sort((a, b) => a - b);
-}
-/*
-function findDifferences(array1,array2){
-  var joinedArr= array1.concat(array2);
-  var results=[];
-  for (var i = 0; i < joinedArr.length - 1; i++) {
-  if (joinedArr[i + 1] == joinedArr[i]) {
-      results.push(joinedArr[i]);
-  } 
-}
-}*/
-
-///// Sum and Product
-function SumAndProduct(input) {
-  var sum = input.reduce((a, b) => a + b);
-  var product = input.reduce((a, b) => a * b);
-  return { sum: sum, product: product }
-}
-//// addItems
-function addItems(items) {
-  var emptyArr = [];
-  var result = emptyArr.concat(items);
-  return result;
-}
 
 //// generateArrayLength
 function generateArrayLength(input1, input2) {
