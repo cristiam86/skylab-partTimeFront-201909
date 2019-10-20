0c9eec9c284343de91180465549e34096fd880b2
Xavier Pascual Betrán <xavierpascualbetran@iMac-de-Xavier.local>
2019-10-20 20:13:55 +0200

    hangman v1

---
+++ staff/xavi/week-4/hangman/game.js
@@ -0,0 +1,49 @@
+var cities = [
+    "Berlin",
+    "Barcelona",
+    "Amsterdam",
+    "Paris",
+    "Florencia",
+    "Edinburgo",
+    "Glasgow",
+    "Zurich",
+    "Roma",
+    "Madrid",
+    "Frankfurt",
+    "Napoles",
+    "Londres",
+    "Moscu",
+    "Estambul",
+    "Liverpool",
+    "Manchester",
+    "Lisboa",
+    "Budapest",
+    "Atenas"
+]
+
+// Método para seleccionar una ciudad random.
+var selectCitie = cities[Math.floor(Math.random() * cities.length)];
+console.log(selectCitie)
+
+
+
+var check = prompt('escriba una letra')
+
+var selectW = check;
+console.log(selectW)
+
+if(selectCitie.includes(selectW)){
+    console.log('Contiene la letra ' + selectW)
+} else{
+    console.log('no')
+}
+/*
+for(x = 0; x < selectCitie.length; x++){
+    if()
+}
+*/
+var wordToGuess = selectCitie.split('');
+
+var guessing = wordToGuess.map(function(){return '__'})
+
+console.log('Ciudad seleccionada:  ' + guessing)
