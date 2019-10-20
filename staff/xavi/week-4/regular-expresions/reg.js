7dd629bb13327aab51f0030d25de2eac65b31fe2
Xavier Pascual Betrán <xavierpascualbetran@iMac-de-Xavier.local>
2019-10-20 18:26:46 +0200

    examples RegExp

--- staff/xavi/week-4/regular-expresions/reg.js
+++ staff/xavi/week-4/regular-expresions/reg.js
@@ -9,11 +9,16 @@
 // /patron/ g - global: le indicamos que siga buscando una vez ha encontrado la primera coincidencia.
 // /patron/ i - insensitive - Ignora en la búsqueda, mayusculas y minusculas.
 // /xav..r/ . - puntos - sustituyen a cualquier caracter.
-// \ - barra invertida 
-// Ejemplo 1:
+// \ - barra invertida - para buscar un caracter especial. Ejemplo: .() {}
+// [aeiou] Listas - Busca todas las vocales del texto
+// [a-z] Rangos - busca todas las letras en minúscula
+// cadena completa () - (12)
+// cadena diferentes elementos (texto|texto), evalúa si hay los dos en el texto.
 
-
-console.log('funciona web ') 
+// delimitadores
+// ^ - No puede haber nada delante.
+// $ - No puede haber nada después.
+ 
 
 var varText = document.getElementById('texto').textContent
 
@@ -24,10 +29,16 @@ var regEx = /esto/gi;
 console.log(regEx.test(varText))
 
 
+/*************************************************************************** */
+
+
 // forma 2 para realizarlo.
 var regEx2 = new RegExp('Esto', 'gi')
 // Evaluación:
 console.log(regEx2.test(varText))
+
+
+/*************************************************************************** */
 
 
 // Evalúo caracteres especiales:
@@ -35,3 +46,67 @@ console.log(regEx2.test(varText))
 var regExp3 = /\(ahora\)/gi;
 // Evaluación:
 console.log('Evalúo barra inveritda ' + regExp3.test(varText))
+
+
+/*************************************************************************** */
+
+
+// Evaluar una imagen:
+var reg4 = /\.png/gi;
+console.log('Evalúo la imagen: ' + reg4.test(varText));
+
+
+/*************************************************************************** */
+
+
+var reg5 = /[a-z]/g;
+console.log('Busca todas las minúsculas ' + reg5.test(varText))
+// En este caso devuelve un true. 
+
+
+/*************************************************************************** */
+
+
+var reg6 = /(38 años)/g
+console.log('evalúo la edad: ' + reg6.test(varText))
+// Me devuelve true.
+
+
+/*************************************************************************** */
+
+
+var reg7 = /(texto|para|pongo)/
+console.log('evalúo diferentes elementos del texto ' + reg7.test(varText))
+// Los evalúa todos. si se cuple solo uno, devuelve true
+
+
+
+
+
+/*************************************************************************** */
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+  
+  function validate() {
+    var $result = $("#result");
+    var email = $("#email").val();
+    $result.text("");
+  
+    if (validateEmail(email)) {
+      $result.text(email + " is valid :)");
+      $result.css("color", "green");
+    } else {
+      $result.text(email + " is not valid :(");
+      $result.css("color", "red");
+    }
+    return false;
+  }
+  
+  $("#validate").on("click", validate);
+
+
+
+
