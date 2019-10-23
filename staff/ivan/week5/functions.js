3a167bc6e65424b736e2c0eedcc66a6d237fd19c
ivandax <ivandax_89@hotmail.com>
2019-10-23 20:57:27 +0200

    worked on inheritance class exercises

--- staff/ivan/week5/functions.js
+++ staff/ivan/week5/functions.js
@@ -4,12 +4,25 @@ var Car = function(name){
     this.name = name;
 }
 
-var c = new Car("Optra");
-console.log(c);
+var c1 = new Car("Optra");
+var c2 = new Car("Elantra");
 
 //prototype adds to the objects __proto__ attribute.
 Car.prototype.start = function(){
     return "Engine of "+this.name+" starting...";
 }
 
-console.log(c.start());
+console.log(c1.start());
+
+//call allows us to take a different scope!!
+console.log(c2.start.call(c1));
+
+//
+
+function Tesla(model){
+    Car.call(this,"Rampage");
+    this.model = model;
+}
+
+var t = new Tesla("model 1");
+console.log(t);
--- staff/ivan/week5/inheritance/functions.js
+++ staff/ivan/week5/inheritance/functions.js
@@ -1,1 +1,42 @@
 console.log("inheritance")
+
+var Person = function(name,last,age,gender,interests){
+    this.firstname = name;
+    this.lastname = last;
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+    this.greeting = function(){
+        return "Hi, I am "+this.firstname;
+    }
+    this.farewell = function(){
+        return this.firstname + " has left the building. Bye for now!";
+    }
+    this.bio = function(){
+        var string_from_interests = this.interests.join(", ")+".";
+        return this.firstname + " " + this.lastname +" is "+this.age+" years old. "+this.gender+" likes "+string_from_interests;
+    }
+}
+
+var morty = new Person("Morty","Smith",14,"He",["Robots","Jessica","Adventures"]);
+console.log("Should log Morty",morty);
+console.log(morty.greeting());
+console.log(morty.farewell());
+console.log(morty.bio());
+
+//now, we create a new class from Person...
+
+function Student(name,last,age,gender,interests){
+    Person.call(this,name,last,age,gender,interests);
+}
+
+//this allows us to say that student is linked to Person's prototype.
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Person;
+Student.prototype.greeting = function(){
+    return "THIS IS "+this.firstname;
+}
+
+var birdperson = new Student("Birdperson","Feathers",45,"He",["flying, traveling"]);
+console.log("Should log Birdperson ",birdperson);
+console.log(birdperson.__proto__.greeting());
