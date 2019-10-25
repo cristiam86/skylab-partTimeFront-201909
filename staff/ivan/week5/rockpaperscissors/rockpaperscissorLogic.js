c0c6fcd07d04aa159205b9b49772f77319dfbf08
ivandax <ivandax_89@hotmail.com>
2019-10-25 18:24:34 +0200

    added the teache's code to the rps

--- staff/ivan/week5/rockpaperscissors/rockpaperscissorLogic.js
+++ staff/ivan/week5/rockpaperscissors/rockpaperscissorLogic.js
@@ -2,18 +2,34 @@ var initGame = function(){
     return {gamesPlayed:0,score1:0,score2:0}
 }
 
-var play = function(selection1,selection2){
-    var options = ['rock','paper','scissors'];
-    var trial = function(a,b){
-    switch(true){
-        case selection1==="rock" && selection2==="paper":
-            return "hand1";
-        case selection1==="paper" && selection2==="rock":
-            return "hand2";
-        default:
-            console.log("a");
-        }
+var WINS = {
+    "paper":"rock",
+    "scissors":"paper",
+    "rock":"scissors"
+}
+
+var PLAYS = ["rock","paper","scissors"];
+
+var areValidHands = function(hand1,hand2){
+    if(!PLAYS.includes(hand1) || !PLAYS.includes(hand2)){
+        return false;
     }
+    return true;
+}
+
+var checPlay = function(hand1,hand2){
+    if(WINS[hand1]===hand2) return "hand1";
+    else if(WINS[hand2]===hand1) return "hand2";
+    return "tie";
+
+}
+
+var play = function(selection1,selection2){
+
+    if(!areValidHands(selection1,selection2)){
+        return "error!";
+    }
+    return checkPlay(selection1,selection);
 }
 
 module.exports = {initGame, play}
