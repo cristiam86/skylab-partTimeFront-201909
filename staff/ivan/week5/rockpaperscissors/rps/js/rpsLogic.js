b5e705683d00271a9dd0555e5109e6e6ccd01970
ivandax <ivandax_89@hotmail.com>
2019-10-27 09:38:32 +0100

    main rps working OK

--- staff/ivan/week5/rockpaperscissors/rps/js/rpsLogic.js
+++ staff/ivan/week5/rockpaperscissors/rps/js/rpsLogic.js
@@ -1,6 +1,3 @@
-// var initGame = function(){
-//     return {gamesPlayed:0,score1:0,score2:0}
-// }
 const WINS = {
     "paper":"rock",
     "scissors":"paper",
@@ -9,21 +6,21 @@ const WINS = {
 
 const PLAYS = ["rock","paper","scissors"];
 
-var areValidHands = function(hand1,hand2){
+var areValidHands = (hand1,hand2)=>{
     if(!PLAYS.includes(hand1) || !PLAYS.includes(hand2)){
         return false;
     }
     return true;
 }
 
-var checkPlay = function(hand1,hand2){
+var checkPlay = (hand1,hand2)=>{
     if(WINS[hand1]===hand2){
         return ["hand1",hand1]; //string and selection
     }
     else if(WINS[hand2]===hand1){
         return ["hand2",hand2];
     };
-    return "tie";
+    return ["tie","tie"];
 }
 
 class Game{
@@ -31,21 +28,28 @@ class Game{
         this.gamesPlayed = 0;
         this.score1 = 0
         this.score2 = 0;
+        this.frequency = {
+            "rock":0,
+            "paper":0,
+            "scissors":0,
+            "tie":0
+        }
     }
 
     play(hand1,hand2){
-        this.gamesPlayed += 1;
         if(!areValidHands(hand1,hand2)){
             return "error!";
         }
+        this.gamesPlayed += 1;
         this.winningHand = checkPlay(hand1,hand2);
         if(this.winningHand[0]==="hand1"){
             this.score1 += 1;
         } else if(this.winningHand[0]==="hand2"){
             this.score2 += 1;
         }
+        this.frequency[this.winningHand[1]] += 1;
         return this.winningHand;
     }
 }
 
-module.exports = {initGame, play}
+module.exports = {Game}
--- staff/ivan/week5/rockpaperscissors/rps/js/rpsUi.js
+++ staff/ivan/week5/rockpaperscissors/rps/js/rpsUi.js
@@ -4,24 +4,31 @@ const get = (id) => {
     return elem;
 }
 
-const updateResults = (obj) => {
+const updateResults = (obj, hand1, hand2) => {
     get("gamesPlayed").innerHTML = obj.gamesPlayed;
     get("score1").innerHTML = obj.score1;
-    get("score2").innerHTML = obj.score2;   
+    get("score2").innerHTML = obj.score2; 
+    get("freqRock").innerHTML = `${obj.frequency["rock"]} times won. ${((obj.frequency["rock"]/obj.gamesPlayed)*100).toFixed(2)}% ocurrence`;
+    get("freqPaper").innerHTML = `${obj.frequency["paper"]} times won. ${((obj.frequency["paper"]/obj.gamesPlayed)*100).toFixed(2)}% ocurrence`;
+    get("freqScissors").innerHTML = `${obj.frequency["scissors"]} times won. ${((obj.frequency["scissors"]/obj.gamesPlayed)*100).toFixed(2)}% ocurrence`
+    get("freqTie").innerHTML = `${obj.frequency["tie"]} results. ${((obj.frequency["tie"]/obj.gamesPlayed)*100).toFixed(2)}% ocurrence`
+    get("hand1play").innerHTML = hand1;
+    get("hand2play").innerHTML = hand2;
 }
 
-let temp = ['rock','scissors'];
+const randomPlay = (array) => { //returns an integer from 0 to 2
+    return array[Math.floor(Math.random()*3)];
+}
 
 //EXECUTION...
 
 //makes instance
 const g = new Game();
 
-console.log(g);
-
-get('run').addEventListener("click", function(){
-    currentWinner = g.play(temp[0], temp[1]);
-    updateResults(g);
+get('run').addEventListener("click", ()=>{
+    let [hand1,hand2] = [randomPlay(PLAYS),randomPlay(PLAYS)] //ES6
+    g.play(hand1,hand2); 
+    updateResults(g,hand1,hand2);
 });
 
 
