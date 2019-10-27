9a7183d43a0c7dddcbc91ab733332d6ce34b84d6
Xavier Pascual Betrán <xavierpascualbetran@iMac-de-Xavier.local>
2019-10-27 12:02:58 +0100

    RPS test 1 done

--- staff/xavi/week-5/TDD/RPS/rps-logic.js
+++ staff/xavi/week-5/TDD/RPS/rps-logic.js
@@ -1,56 +1,97 @@
-var game =  {
-    gamesplayer: 0,
-    score1: 0,
-    score2: 0,
-}
 
-function initGame(){
-    return  {
-        gamesplayer: 0,
-        score1: 0,
-        score2: 0,
+function initGame() {
+    return {
+      //gamesPlayed: 0,
+      gamesNumber: 0,
+      //score1: 0,
+      ResultA: 0,
+      //score2: 0,
+      ResultB: 0,
+      //lastPlayWinner: null,
+      playerWin: null,
     }
-}
+  }
+  
+  // WINS
+  // jugadas ganadoras
+  var JUGADASGANADORAS = {
+    'paper': 'rock',
+    'scissors': 'paper',
+    'rock': 'scissors'
+  };
+  
+  //var PLAYS = ['rock', 'paper', 'scissors'];
+  // tipos de manos o jugadas
+  var JUGADAS = ['paper', 'rock', 'scissors'];
 
 
-function play(hand1, hand2){
+  // validador de jugadores
+  /*function areValidHands(hand1, hand2) {
+    if(!PLAYS.includes(hand1) || !PLAYS.includes(hand2)) {
+      return false;
+    }
+    return true;
+  } */
 
 
-        // hand 1
-        if(hand1 === 'paper' && hand2 === 'rock'){
-            return 'hand1'
-
+// FUNCIÓN VALIDADORA
+ function validadorJugadores(hand1, hand2) {
+        if(JUGADAS.includes(hand1) || JUGADAS.includes(hand2)) {
+        return true;
         }
-        if(hand1 === 'rock' && hand2 === 'scissors'){
-            return 'hand1'
+        return false;
+    } 
 
-        }
-        if(hand1 === 'scissors' && hand2 === 'paper'){
-            return 'hand1'
-
-        }
-        // hand 2
-        if(hand1 === 'rock' && hand2 === 'paper'){
-            return 'hand2'
-
-        }
-        if(hand1 === 'scissors' && hand2 === 'rock'){
-            return 'hand2'
-
-        }
-        if(hand1 === 'paper' && hand2 === 'scissors'){
-            return 'hand2'
-
-        }
-        // tie
-        if( hand1 === hand2 ){
-            return 'tie'
-        }
-
+  
+  function checkPlay(playerA, playerB) {
+    // si la gugada ganadora sobre playerA es igual a player b = retorna PLayerA como ganador
+    if(JUGADASGANADORAS[playerA] === playerB) return 'playerA';
+    else if(JUGADASGANADORAS[playerB] === playerA) return 'playerB';
     
-}
-
-module.exports = {
+    return 'tie';
+  }
+  
+  function play(hand1, hand2, gameState) {
+    if (validadorJugadores(hand1, hand2)) {
+      return {
+        gamesPlayed: gameState.gamesPlayed + 1,
+        score1: gameState.score1,
+        score2: gameState.score2,
+        lastPlayWinner: null,
+        lastPlayError: true
+      }
+    }
+    var winner = checkPlay(hand1, hand2);
+    if(winner === 'tie') {
+      return {
+        gamesPlayed: gameState.gamesPlayed + 1,
+        score1: gameState.score1,
+        score2: gameState.score2,
+        lastPlayWinner: 'tie',
+        lastPlayError: false
+      }
+    } else if(winner === 'hand1') {
+      return {
+        gamesPlayed: gameState.gamesPlayed + 1,
+        score1: gameState.score1 + 1,
+        score2: gameState.score2,
+        lastPlayWinner: 'hand1',
+        lastPlayError: false
+      }
+    } else {
+      return {
+        gamesPlayed: gameState.gamesPlayed + 1,
+        score1: gameState.score1,
+        score2: gameState.score2 + 1,
+        lastPlayWinner: 'hand2',
+        lastPlayError: false
+      }
+    }
+  }
+  
+  const gameLogic = {
     initGame,
     play
-}
+  };
+  module.exports = gameLogic;
+  
--- staff/xavi/week-5/TDD/RPS/rps-logic.test.js
+++ staff/xavi/week-5/TDD/RPS/rps-logic.test.js
@@ -1,58 +1,132 @@
-var rockPaperScissorsLogic = require('./rps-logic')
-
-describe('title game', function(){
-
-    
-    test('init Game', function(){
-        var game = rockPaperScissorsLogic.initGame();
-        expect(game.gamesplayer).toBe(0);
-        expect(game.score1).toBe(0);
-        expect(game.score2).toBe(0);
-    });
-
-    test('gana la mano uno', function(){
-        var result = rockPaperScissorsLogic.play('paper', 'rock')
-        expect(result).toBe('hand1');
-    });
-
-    test('gana la mano uno', function(){
-        var result = rockPaperScissorsLogic.play('rock', 'scissors')
-        expect(result).toBe('hand1');
-    });
-
-    test('gana la mano uno', function(){
-        var result = rockPaperScissorsLogic.play('scissors', 'paper')
-        expect(result).toBe('hand1');
-    });
-
-    test('gana la mano dos', function(){
-        var result = rockPaperScissorsLogic.play('rock', 'paper')
-        expect(result).toBe('hand2');
-    });
-
-    test('gana la mano dos', function(){
-        var result = rockPaperScissorsLogic.play('scissors', 'rock',)
-        expect(result).toBe('hand2');
-    });
-
-    test('gana la mano dos', function(){
-        var result = rockPaperScissorsLogic.play('paper', 'scissors')
-        expect(result).toBe('hand2');
-    });
 
 
-    test('empate', function(){
-        var result = rockPaperScissorsLogic.play('rock', 'rock')
-        expect(result).toBe('tie');
-    });
 
-    test('empate', function(){
-        var result = rockPaperScissorsLogic.play('scissors', 'scissors',)
-        expect(result).toBe('tie');
-    });
+// la variable rockPaperScissorsLogic  es requerida por la logica del juego 
+// ********* VINCULO ENTRE LOGICA Y TEST JS.
+var rockPaperScissorsLogic = require('./rps-logic');
 
-    test('empate', function(){
-        var result = rockPaperScissorsLogic.play('paper', 'paper')
-        expect(result).toBe('tie');
-    });
+
+// MIRAR SI SE UTILIZA
+
+/*
+var gameState;
+beforeEach(function() {
+  gameState = rockPaperScissorsLogic.initGame();
+})
+*/
+
+
+
+
+// test 1: Se inicia el juego con las variables globales:
+
+// Titulo del test:
+describe('RPS INICIO', function(){
+  // este es el mensaje que devuelve si pasa el test
+  test('EL juego se ha iniciado correctamente! ', function(){
+    // Se declara una variable = game.
+    // La variable game es igual la variable de vinculo test/logic y se le pasa la función del archivo de logica .initgame()
+    var game = rockPaperScissorsLogic.initGame();
+
+    // dentro de la función init game, se declaran las siguientes varibles
+    // logica expect-toBe = expect(variable).se espera que sea(dato)
+    // Si cambia el toBe(0) - el trst peta
+    expect(game.gamesNumber).toBe(0);
+    expect(game.ResultA).toBe(0);
+    expect(game.ResultB).toBe(0);
+    expect(game.playerWin).toBe(null);
+  });
+})
+
+
+
+
+
+describe('RPS -> JUGADOR A gana ', function(){
+  test('jugdor A gana con -> paper', function(){
+    var result = rockPaperScissorsLogic.play('paper', 'rock', gameState);
+    expect(result.lastPlayWinner).toBe('playerA');
+  })
+  test('should win hand1 -> rock', function(){
+    var result = rockPaperScissorsLogic.play('rock', 'scissors', gameState);
+    expect(result.lastPlayWinner).toBe('playerA');
+  })
+  test('should win hand1 -> scissors', function(){
+    var result = rockPaperScissorsLogic.play('scissors', 'paper', gameState);
+    expect(result.lastPlayWinner).toBe('playerA');
+  })
+})
+  
+describe('RPS HAND2 WINS', function(){
+  test('should win hand2 -> paper', function(){
+    var result = rockPaperScissorsLogic.play('rock', 'paper', gameState);
+    expect(result.lastPlayWinner).toBe('hand2');
+  })
+  test('should win hand2 -> rock', function(){
+    var result = rockPaperScissorsLogic.play('scissors', 'rock', gameState);
+    expect(result.lastPlayWinner).toBe('hand2');
+  })
+  test('should win hand2 -> scissors', function(){
+    var result = rockPaperScissorsLogic.play('paper', 'scissors', gameState);
+    expect(result.lastPlayWinner).toBe('hand2');
+  })
+});
+
+describe('RPS GAME TIED', function(){
+  test('should end tied hands -> paper', function(){
+    var result = rockPaperScissorsLogic.play('paper', 'paper', gameState);
+    expect(result.lastPlayWinner).toBe('tie');
+  })
+  test('should end tied hands -> rock', function(){
+    var result = rockPaperScissorsLogic.play('rock', 'rock', gameState);
+    expect(result.lastPlayWinner).toBe('tie');
+  })
+  test('should end tied hands -> scissors', function(){
+    var result = rockPaperScissorsLogic.play('scissors', 'scissors', gameState);
+    expect(result.lastPlayWinner).toBe('tie');
+  })
+})
+
+describe('RPS INCORRECT HANDS', function(){
+  test('should return error by hand1', function(){
+    var result = rockPaperScissorsLogic.play('dd', 'paper', gameState);
+    expect(result.lastPlayError).toBe(true);
+  })
+  test('should return error by hand2', function(){
+    var result = rockPaperScissorsLogic.play('paper', 'pp', gameState);
+    expect(result.lastPlayError).toBe(true);
+  })
+})
+
+describe('ACCUMULATES GAMES PLAYED', function(){
+  test('should sum 1 game played when error', function() {
+    var newGameState = rockPaperScissorsLogic.play('tt', 'paper', gameState);
+    expect(newGameState.gamesPlayed).toBe(1);
+    var finalGameState = rockPaperScissorsLogic.play('paper', 'tt', newGameState);
+    expect(finalGameState.gamesPlayed).toBe(2);
+  })
+
+  test('shoudl sum score to hand 1', function(){
+    var newGameState = rockPaperScissorsLogic.play('paper', 'rock', gameState);
+    expect(newGameState.score1).toBe(1);
+    expect(newGameState.score2).toBe(0);
+    expect(newGameState.lastPlayWinner).toBe('hand1');
+    expect(newGameState.lastPlayError).toBe(false)
+  })
+
+  test('shoudl sum score to hand 2', function(){
+    var newGameState = rockPaperScissorsLogic.play('scissors', 'rock', gameState);
+    expect(newGameState.score1).toBe(0);
+    expect(newGameState.score2).toBe(1);
+    expect(newGameState.lastPlayWinner).toBe('hand2');
+    expect(newGameState.lastPlayError).toBe(false)
+  })
+
+  test('shouldn\'t sum scores when tie', function() {
+    var newGameState = rockPaperScissorsLogic.play('rock', 'rock', gameState);
+    expect(newGameState.score1).toBe(0);
+    expect(newGameState.score2).toBe(0);
+    expect(newGameState.lastPlayWinner).toBe('tie')
+    expect(newGameState.lastPlayError).toBe(false)
+  })
 })
