b5d331f318653b3b5e85f261fec5990d06b7bdf7
eugferman <eugferman@gmail.com>
2019-10-28 22:04:35 +0100

    hangman html funcionando

--- staff/eugenio/week5/hangman/testhangman.js
+++ staff/eugenio/week5/hangman/testhangman.js
@@ -1,8 +1,7 @@
 const hangmanWords = ['Marcelo', 'Modric', 'Courtois', 'Hazard', 'Benzema', 'Casemiro', 'Varane', 'Carvajal', 'Vinicius']
-
-    let wordRandom = Math.floor(Math.random() * (9 - 0)) + 0;
-    let word = hangmanWords[wordRandom]
-    let wordToGuess = word.split('');
-    let guessing = wordToGuess.map(function(){return '_'});
-
-console.log(guessing.join(' '));  
+            let wordRandom = Math.floor(Math.random() * (9 - 0)) + 0;
+            let word = hangmanWords[wordRandom]
+            let wordToGuess = word.split('');
+            let guessing = wordToGuess.map(function(){return '_'});
+            let lives = word.length -2;
+            console.log(Array.from(word));
