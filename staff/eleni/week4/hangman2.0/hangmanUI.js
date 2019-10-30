51a7a81ed63ff3e595973c294c37dd3085dc839e
Suneleta <elenhtakh@gmail.com>
2019-10-30 18:58:29 +0100

    Added test

--- staff/eleni/week4/hangman2.0/hangmanUI.js
+++ staff/eleni/week4/hangman2.0/hangmanUI.js
@@ -6,11 +6,11 @@ function setGameUiStatus(gameLogicState) {
     newWord = newWord.join(' ')
     $_id('word-to-guess').innerHTML = newWord;
     $_id('input-guess').value = gameLogicState.input;
-    $_id('error-text').innerHTML = "You lost";
+    $_id('error-text').innerHTML = gameLogicState.errorText;
     $_id('success-text').innerHTML = gameLogicState.successText;
     $_id('stickman').innerHTML = "<img src=\"https://media.giphy.com/media/5VKbvrjxpVJCM/giphy.gif\" width=\"400px\" height=\"150px\">";
     $_id('guess-form').addEventListener('submit', checkLetter);
-    $_id('submit-button').addEventListener('click', guessWord);
+    //  $_id('submit-button').addEventListener('click', guessWord);
     $_id('reset-button').addEventListener('click', initGame, renewWord);
 }
 
