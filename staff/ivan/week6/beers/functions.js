b8f6b03c2317c7f8352a9f3e8c4ec51ed1dd768a
ivandax <ivandax_89@hotmail.com>
2019-10-31 21:01:33 +0100

    api beer project, started, added main beer list builder

--- staff/ivan/week6/beers/functions.js
+++ staff/ivan/week6/beers/functions.js
@@ -1,1 +1,44 @@
 console.log("beers")
+
+//UI functions...
+
+const get = (id) => {
+    return document.getElementById(id);
+}
+
+const addBeer = (beer, where) => {
+    const node = document.createElement("div");
+    node.setAttribute("id", beer.id);
+    const textnode = document.createTextNode(beer.name);
+    node.appendChild(textnode);
+    get(where).appendChild(node);
+}
+
+const buildBeerList = ()=>{
+    axios.get('https://api.punkapi.com/v2/beers?page=1').then(response => {
+        const beersObj = response;
+        console.log(beersObj.data);
+        beersObj.data.forEach(element => {
+            addBeer(element,"list");           
+        });
+    })
+}
+
+buildBeerList();
+
+// this approach using fetch also worked... but we prefer to use axios which is easier.
+
+// const response = fetch('https://api.punkapi.com/v2/beers?page=1');
+// const responseObj = response.then(function(resp) {
+//     const json = resp.json(); //when then replies, make the json variable
+//     console.log("Received first response :", response);
+//     console.log("The type of the first response is: ", typeof(json));
+//     return json;
+// });
+// const responseJson = responseObj.then(function(myJson) {
+//     console.log("type of the second response is: ", typeof(myJson));
+//     console.log("Received second response :", myJson);
+//     for(let i in myJson){
+//         addElement("div", myJson[i].name, "display"); 
+//     }
+// });
